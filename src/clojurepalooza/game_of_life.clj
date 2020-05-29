ce0bd4a15a4a009b748ef26942c7de8b11a7219a
spencerminer <sminer@opploans.com>
2020-05-29 16:02:42 -0500

    Game of Life, pair programmed with Joey Bloom (@15joeybloom)

---
+++ src/clojurepalooza/game_of_life.clj
@@ -0,0 +1,61 @@
+(ns clojurepalooza.game-of-life)
+
+;; 4clojure problem 94 http://www.4clojure.com/problem/94
+
+(defn next-gen [board]
+  (let [live? #(= \# %)
+        live-neighbors
+        (fn [i j]
+          (count
+            (filter live?
+                    (for [x [-1 0 1]
+                          y [-1 0 1]
+                          :when (not (and (zero? x) (zero? y)))]
+                      (get-in board [(+ i x) (+ j y)])))))
+        rows (count board)
+        cols (count (first board))]
+    (vec
+      (for [i (range rows)]
+        (apply str
+               (for [j (range cols)]
+                 (let [good-neighbor-count?
+                       (if (live? (get-in board [i j]))
+                         #{2 3} #{3})]
+                   (if (good-neighbor-count? (live-neighbors i j))
+                     \#
+                     \space))))))))
+
+(def block ["    "
+            " ## "
+            " ## "
+            "    "])
+
+(def blinker ["     "
+              "     "
+              " ### "
+              "     "
+              "     "])
+
+(def beacon ["      "
+             " ##   "
+             " ##   "
+             "   ## "
+             "   ## "
+             "      "])
+
+(def other-beacon ["      "
+                   "      "
+                   "  ### "
+                   " ###  "
+                   "      "
+                   "      "])
+
+(comment
+
+  (doseq [game-round (take 10 (iterate next-gen blinker))]
+    (dorun
+      (map #(prn %)
+           game-round))
+    (Thread/sleep 1000))
+
+  )
